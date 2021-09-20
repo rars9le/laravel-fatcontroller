1c4f914e9d7fcf45ffed02470ca1f93ab95baad2
rars9le <le9240@gmail.com>
2021-09-21 08:58:44 +0900

    add lib dir and get preview

---
+++ src/app/Lib/LinkPreview/LinkPreview.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Lib\LinkPreview;
+
+use Dusterio\LinkPreview\Client;
+
+use App\Lib\LinkPreview\GetLinkPreviewResponse;
+
+final class LinkPreview
+{
+    public function get(string $url): GetLinkPreviewResponse
+    {
+        $previewClient = new Client($url);
+        $response = $previewClient->getPreview('general')->toArray();
+
+        return new GetLinkPreviewResponse($response['title'], $response['description'], $response['cover']);
+    }
+}
